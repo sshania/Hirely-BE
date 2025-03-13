905203943e26d5cd6da6a96e728d5531be60f213
Shania Sukandar <shaniasukandar88@gmail.com>
2025-03-13 23:11:37 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello Guys. Ini API Hirely"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "query": q}
