8f7bbc71e644372bdea3a0a38237ce9ae88dab1e
HardianRaffaell <161046492+HardianRaffaell@users.noreply.github.com>
2025-05-20 17:24:10 +0700

    validation for reset password
    add validation for reset password

--- authetification.py
+++ authetification.py
@@ -58,6 +58,13 @@ class ResetPasswordRequest(BaseModel):
     new_password: str
     confirm_password: str
 
+    @field_validator("new_password")
+    @classmethod
+    def validate_password(cls, v):
+        if len(v) < 8:
+            raise ValueError("Password must be at least 8 characters long.")
+        return v
+
     @model_validator(mode="after")
     def passwords_match(self):
         if self.new_password != self.confirm_password:
